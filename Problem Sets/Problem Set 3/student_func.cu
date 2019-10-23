08afeade7cc28ef2096a7641d2712b08b6bc8c2d
joel <wuxibin89@163.com>
2019-10-23 22:48:40 +0800

    finish problem set 3

--- Problem Sets/Problem Set 3/student_func.cu
+++ Problem Sets/Problem Set 3/student_func.cu
@@ -6,7 +6,7 @@
 
   A High Dynamic Range (HDR) image contains a wider variation of intensity
   and color than is allowed by the RGB format with 1 byte per channel that we
-  have used in the previous assignment.  
+  have used in the previous assignment.
 
   To store this extra information we use single precision floating point for
   each channel.  This allows for an extremely wide range of intensity values.
@@ -53,7 +53,7 @@
   Old TV signals used to be transmitted in this way so that black & white
   televisions could display the luminance channel while color televisions would
   display all three of the channels.
-  
+
 
   Tone-mapping
   ============
@@ -80,16 +80,132 @@
 */
 
 #include "utils.h"
+#include <limits>
 
-void your_histogram_and_prefixsum(const float* const d_logLuminance,
-                                  unsigned int* const d_cdf,
-                                  float &min_logLum,
-                                  float &max_logLum,
-                                  const size_t numRows,
-                                  const size_t numCols,
-                                  const size_t numBins)
-{
-  //TODO
+template <typename reduce_op>
+__global__ void reduce_kernel(const float *const d_logLuminance,
+                              float *d_output, const size_t numElems,
+                              reduce_op op) {
+  int gid = threadIdx.x + blockIdx.x * blockDim.x;
+  int tid = threadIdx.x;
+
+  extern __shared__ float s_data[];
+
+  if (gid < numElems) {
+    s_data[tid] = d_logLuminance[gid];
+  }
+  __syncthreads();
+
+  int offset = blockIdx.x * blockDim.x;
+  int nsize =
+      (offset + blockDim.x < numElems) ? blockDim.x : (numElems - offset);
+  while (nsize > 1) {
+    int len = (nsize + 1) / 2;
+    if (tid < nsize / 2) {
+      s_data[tid] = op(s_data[tid], s_data[tid + len]);
+    }
+    nsize = len;
+    __syncthreads();
+  }
+
+  if (tid == 0) {
+    d_output[blockIdx.x] = s_data[0];
+  }
+}
+
+template <typename reduce_op>
+float reduce(const float *const d_logLuminance, int numElems, float init,
+             const reduce_op &op) {
+  int block_size = 1024;
+  int grid_size = (numElems + block_size - 1) / block_size;
+
+  float *d_output;
+  checkCudaErrors(cudaMalloc(&d_output, grid_size * sizeof(float)));
+
+  reduce_kernel<<<grid_size, block_size, block_size * sizeof(float)>>>(
+      d_logLuminance, d_output, numElems, op);
+  cudaDeviceSynchronize();
+  checkCudaErrors(cudaGetLastError());
+  printf("first round, grid_size: %d\n", grid_size);
+
+  numElems = grid_size;
+  for (; numElems >= block_size; numElems = grid_size) {
+    grid_size = (numElems + block_size - 1) / block_size;
+    printf("grid_size: %d\n", grid_size);
+
+    float *d_input = d_output;
+    checkCudaErrors(cudaMalloc(&d_output, grid_size * sizeof(float)));
+
+    reduce_kernel<<<grid_size, block_size, block_size * sizeof(float)>>>(
+        d_input, d_output, numElems, op);
+    cudaDeviceSynchronize();
+    checkCudaErrors(cudaGetLastError());
+
+    checkCudaErrors(cudaFree(d_input));
+  }
+
+  printf("grid_size: %d, numElems: %d\n", grid_size, numElems);
+  float *h_output = new float[numElems];
+  checkCudaErrors(cudaMemcpy(h_output, d_output, numElems * sizeof(float),
+                             cudaMemcpyDeviceToHost));
+  cudaFree(d_output);
+
+  float res = init;
+  for (int i = 0; i < numElems; ++i) {
+    res = op(res, h_output[i]);
+  }
+  delete[] h_output;
+
+  return res;
+}
+
+__global__ void histogram(const float *const d_logLuminance, size_t numElems,
+                          unsigned int *const d_cdf, size_t numBins,
+                          float min_logLum, float max_logLum) {
+  int gid = threadIdx.x + blockIdx.x * blockDim.x;
+  if (gid < numElems) {
+    int bin = (d_logLuminance[gid] - min_logLum) / (max_logLum - min_logLum) *
+              numBins;
+    atomicAdd(&d_cdf[bin], 1);
+  }
+}
+
+// Hillis & Steele inclusive scan
+__global__ void HS_scan(unsigned int *const d_cdf, size_t numBins) {
+  int gid = threadIdx.x + blockIdx.x * blockDim.x;
+
+  if (gid == 0) {
+    int init = 0;
+    for (int i = 0; i < numBins; ++i) {
+      init += d_cdf[i];
+      d_cdf[i] = init;
+    }
+  }
+
+  // TODO
+}
+
+// Blelloch exclusive scan
+__global__ void Blelloch_scan(unsigned int *const d_cdf, size_t numBins) {
+  int gid = threadIdx.x + blockIdx.x * blockDim.x;
+
+  if (gid == 0) {
+    int init = 0;
+    for (int i = 0; i < numBins; ++i) {
+      int temp = d_cdf[i];
+      d_cdf[i] = init;
+      init += temp;
+    }
+  }
+
+  // TODO
+}
+
+void your_histogram_and_prefixsum(const float *const d_logLuminance,
+                                  unsigned int *const d_cdf, float &min_logLum,
+                                  float &max_logLum, const size_t numRows,
+                                  const size_t numCols, const size_t numBins) {
+  // TODO
   /*Here are the steps you need to implement
     1) find the minimum and maximum value in the input logLuminance channel
        store in min_logLum and max_logLum
@@ -99,6 +215,24 @@ void your_histogram_and_prefixsum(const float* const d_logLuminance,
     4) Perform an exclusive scan (prefix sum) on the histogram to get
        the cumulative distribution of luminance values (this should go in the
        incoming d_cdf pointer which already has been allocated for you)       */
+  min_logLum = reduce(
+      d_logLuminance, numRows * numCols, std::numeric_limits<float>::max(),
+      [] __host__ __device__(float a, float b) { return (a < b) ? a : b; });
 
+  printf("minimum: %f\n", min_logLum);
 
+  max_logLum = reduce(
+      d_logLuminance, numRows * numCols, std::numeric_limits<float>::min(),
+      [] __host__ __device__(float a, float b) { return (a < b) ? b : a; });
+
+  printf("maximum: %f\n", max_logLum);
+
+  int numElems = numRows * numCols;
+  int block_size = 1024;
+  int grid_size = (numElems + block_size - 1) / block_size;
+
+  histogram<<<grid_size, block_size>>>(d_logLuminance, numElems, d_cdf, numBins,
+                                       min_logLum, max_logLum);
+
+  Blelloch_scan<<<1, block_size>>>(d_cdf, numBins);
 }
